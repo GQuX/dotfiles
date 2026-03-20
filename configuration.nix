77c6c095cabab605646424d59467f39320f92ce5
GQuX <github.gqux@gmail.com>
2026-03-20 07:35:44 -0500

    use systemd-bood and efi instead of grub

--- configuration.nix
+++ configuration.nix
@@ -13,9 +13,8 @@ networking.hostName = "thinkpad";
 
 
 # Bootloader.
-boot.loader.grub.enable = true;
-boot.loader.grub.device = "/dev/sda";
-# boot.loader.grub.useOSProber = true;
+boot.loader.systemd-boot.enable = true;
+boot.loader.efi.canTouchEfiVariables = true;
 
 nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
