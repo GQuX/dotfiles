8c9c4e147f211114f6a9e2aaa0ffa675da77c4be
GQuX <github.gqux@gmail.com>
2026-03-21 07:46:04 -0500

    removing symlinks until home-manager is installed

--- home.nix
+++ home.nix
@@ -3,8 +3,8 @@
 home.username = "icarus";
 home.homeDirectory = "/home/icarus";
 
-xdg.configFile."niri/config.kdl".source = ./niri-config.kdl;
-xdg.configFile."noctalia/settings.json".source = ./noctalia-settings.json;
+#xdg.configFile."niri/config.kdl".source = ./niri-config.kdl;
+#xdg.configFile."noctalia/settings.json".source = ./noctalia-settings.json;
 
 home.stateVersion = "25.11";
 
