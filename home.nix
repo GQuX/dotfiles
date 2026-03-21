79158ffe41ae55a1d3e8e58cc5dad4dbb6df7e0e
GQuX <github.gqux@gmail.com>
2026-03-21 07:39:55 -0500

    forgot a semicolon :(

--- home.nix
+++ home.nix
@@ -3,7 +3,7 @@
 home.username = "icarus";
 home.homeDirectory = "/home/icarus";
 
-xdg.configFire."niri/config.kdl".source = ./niri-config.kdl
+xdg.configFire."niri/config.kdl".source = ./niri-config.kdl;
 xdg.configFile."noctalia/settings.json".source = ./noctalia-settings.json;
 
 home.stateVersion = "25.11";
