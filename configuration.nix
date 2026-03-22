6ff5f25f6664867bebd1e7582096501f649e7e5d
GQuX <github.gqux@gmail.com>
2026-03-21 23:20:08 -0500

    install home-manager?

--- configuration.nix
+++ configuration.nix
@@ -1,10 +1,14 @@
 # Edit this configuration file to define what should be installed on
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
-{ config, pkgs, ... }: {
+{ config, pkgs, ... }: let
+  home-manager = builtins.fetchTarball https://github.com/nix-community/home-manager/archive/release-25.11.tar.gz;
+in {
+
 
 imports = [
   ./hardware-configuration.nix # Hardware scan results
+  (import "${home-manager}/nixos")
 ];
 
 system.stateVersion = "25.11";
--- niri.nix
+++ niri.nix
@@ -1,15 +1,14 @@
-{ pkgs, config, inputs, niri, ... }: {
-
-programs.niri.enable = true;
+{ pkgs, config, inputs, ... }: {
 
 home-manager.users.icarus = {
-	programs.niri.settings = {
-		spawn-at-startup = [
-			{ command = [ "${inputs.noctalia.packages.${pkgs.system}.default}/bin/noctalia-shell" ]; }
-		];
+	programs.niri = {
+		package = niri;
+		settings = {
+			spawn-at-startup = [
+				{ command = [ "noctalia-shell" ]; }
+			];
+		};
 	};
-
-	programs.noctalia-shell.enable = true;
 };
 
 }
