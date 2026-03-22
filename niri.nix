e02b7da3d0ce4e2b1948d5b0b2297d20b6fa5003
GQuX <github.gqux@gmail.com>
2026-03-22 00:08:33 -0500

    tell niri flake where noctalia shell is

--- niri.nix
+++ niri.nix
@@ -1,16 +1,16 @@
-{ pkgs, config, inputs, ... }: {
+{ pkgs, config, inputs, ... }: let
+  system = pkgs.stdenv.hostPlatform.system;
+  noctalia-bin = "${inputs.noctalia.packages.${system}.default}/bin/noctalia-shell";
+in {
 
 programs.niri = {
 	enable = true;
-	package = inputs.niri.packages.${pkgs.stdenv.hostPlatform.system}.niri-unstable;
+	package = inputs.niri.packages.${system}.niri-unstable;
 	settings = {
 		spawn-at-startup = [
-			{ command = [ "noctalia-shell" ]; }
+			{ command = [ "${noctalia-bin}" ]; }
 		];
 	};
 };
-
-# If adding it back causes the "Status 4" error again, it just means the noctalia-shell binary isn't in Niri's search path when it starts up. In that case, you'd change the command to use the absolute store path:
-# command = [ "${inputs.noctalia.packages.${pkgs.system}.default}/bin/noctalia-shell" ];
 
 }
