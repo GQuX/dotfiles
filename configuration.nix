ff51541d1a26d22fbf614f66dadd3d43194c7773
GQuX <github.gqux@gmail.com>
2026-03-21 23:53:21 -0500

    remove duplicate niri package import

--- configuration.nix
+++ configuration.nix
@@ -77,8 +77,7 @@ users.users.icarus = {
 
 
 nixpkgs.config.allowUnfree = true;
-# programs.hyprland.enable = true;
-programs.niri.enable = true;
+#programs.niri.enable = true;
 programs.firefox.enable = true;
 
 environment.systemPackages = with pkgs; [
--- niri.nix
+++ niri.nix
@@ -1,6 +1,7 @@
 { pkgs, config, inputs, ... }: {
 
 programs.niri = {
+	enable = true;
 	package = inputs.niri.packages.${pkgs.stdenv.hostPlatform.system}.niri-unstable;
 	settings = {
 		spawn-at-startup = [
