c7cd39a3cfeb952b25fac482dccc0e22fcbf6e7e
GQuX <github.gqux@gmail.com>
2026-03-22 00:17:20 -0500

    testing spawn-at-startup with a working app

--- niri.nix
+++ niri.nix
@@ -1,14 +1,15 @@
 { pkgs, config, inputs, ... }: let
-  system = pkgs.stdenv.hostPlatform.system;
-  noctalia-bin = "${inputs.noctalia.packages.${system}.default}/bin/noctalia-shell";
+  #system = pkgs.stdenv.hostPlatform.system;
+  #noctalia-pkg = inputs.noctalia.packages.${system}.default;
 in {
 
 programs.niri = {
 	enable = true;
-	package = inputs.niri.packages.${system}.niri-unstable;
+	#package = inputs.niri.packages.${system}.niri-unstable;
 	settings = {
 		spawn-at-startup = [
-			{ command = [ "${noctalia-bin}" ]; }
+			{ command = [ "firefox" ]; }
+			#{ command = [ "noctalia-shell" ]; }
 		];
 	};
 };
