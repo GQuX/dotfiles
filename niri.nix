d50cf6be549befb0b4770b22988a81b01db1e3b0
GQuX <github.gqux@gmail.com>
2026-03-21 23:33:13 -0500

    change niri package in niri.nix

--- niri.nix
+++ niri.nix
@@ -1,7 +1,7 @@
 { pkgs, config, inputs, ... }: {
 
 programs.niri = {
-	package = inputs.niri.packages.${pkgs.system}.niri;
+	package = inputs.niri.packages.${pkgs.stdenv.hostPlatform.system}.default;
 	settings = {
 		spawn-at-startup = [
 			{ command = [ "noctalia-shell" ]; }
