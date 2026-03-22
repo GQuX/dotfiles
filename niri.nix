274a7b3641eff020f63b675d062eef852e9a689f
GQuX <github.gqux@gmail.com>
2026-03-21 23:36:54 -0500

    rename package to nixos-unstable (no default)

--- niri.nix
+++ niri.nix
@@ -1,7 +1,7 @@
 { pkgs, config, inputs, ... }: {
 
 programs.niri = {
-	package = inputs.niri.packages.${pkgs.stdenv.hostPlatform.system}.default;
+	package = inputs.niri.packages.${pkgs.stdenv.hostPlatform.system}.niri-unstable;
 	settings = {
 		spawn-at-startup = [
 			{ command = [ "noctalia-shell" ]; }
